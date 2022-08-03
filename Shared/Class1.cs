ba71b34f8a2dff8d1f4f72676aafd3593db3e894
LRJis <1021291084@qq.com>
2022-08-03 15:35:37 +0800

    Init commit.

---
+++ Shared/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shared
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string PassWord { get; set; }
+    }
+
+    public class MessageInfo
+    {
+        public int SenderId { get; set; }
+        public string Content { get; set; }
+        public int ReceiverId { get; set; }
+        public DateTime PublishTime { get; set; }
+    }
+}
